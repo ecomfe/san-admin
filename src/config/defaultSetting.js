89d3694dec570856737c1431fe8a3508fd24e6f4
Chen <chenshaoping01@baidu.com>
2021-10-27 17:59:15 +0800

    fix: restore default themm config

--- src/config/defaultSetting.js
+++ src/config/defaultSetting.js
@@ -1,10 +1,11 @@
-
 module.exports = {
-    'navTheme': 'dark',
-    'primaryColor': '#1890ff',
-    'layout': 'top',
-    'contentWidth': 'Fixed',
-    'splitMenus': false,
-    'colorWeak': false,
-    'sidebarWitdh': 208
+    navTheme: 'light',
+    primaryColor: '#1890ff',
+    layout: 'side',
+    contentWidth: 'Fluid',
+    fixedHeader: false,
+    fixSiderbar: true,
+    splitMenus: false,
+    colorWeak: false,
+    sidebarWitdh: 208
 };
